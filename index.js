4077e790c77de54a0d3ff3ae6b1881a55e09f50b
aaron <aaron@crunchbang.(none)>
2012-11-21 16:06:56 +0000

    Add whitelist. Add blacklist. Reset rate is for individual clients. Closes #2 #3

--- index.js
+++ index.js
@@ -1,69 +1,85 @@
-var clients = {},
-    limit,
-    whitelist;
+var clients   = {},
+    whitelist,
+    blacklist,
+
+    config = {
+      whitelist: {
+        totalRequests: 5000,
+        every:         60 * 1000 * 1000
+      },
+      blacklist: {
+        totalRequests: 0,
+        every:         0 
+      },
+      normal: {
+        totalRequests: 5,
+        every:         60 * 1000 * 1000
+      }
+    };
+
 
 module.exports = function (options) {
-  var options = options        || {};
-  limit       = options.limit  || 5;
-  whitelist   = options.ignore || [];
-  setInterval(clearClients, options.every || 60 * 60 * 1000);
+  var options = options           || {};
+  whitelist   = options.whitelist || [];
+  blacklist   = options.blacklist || [];
+  
+  //TODO: Customizable config
+
   return middleware;
-}
-
-function middleware (req, res, next) {
-  var ip = getClientAddress(req);
-
-  if (whiteListed(ip)) {
-    next();
-  } else {
-    if (!clientExists(ip)) {
-      trackNewClient(ip);
-    }
-
-    tickClient(ip);
-
-
-    res.setHeader('X-RateLimit-Limit', limit);
-    res.setHeader('X-RateLimit-Remaining', clients[ip]);
-
-    if (checkOk(ip)) {
-      console.log(clients);
-      next();
-    } else {
-      clients[ip] = 0;
-      res.end('Rate limit exceeded');
-    }
-  }
 };
 
-function whiteListed (ip) {
-  return whitelist.indexOf(ip) > -1;
-}
+function middleware (req, res, next) {
+  var name   = req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      type   = getClientType(name),
+      client = clients[name];
 
-function getClientAddress(req) {
-  return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-}
-
-function clearClients () {
-  clients = {};
-}
-
-function clientExists (ip) {
-  if (ip in clients) return true;
-  return false;
-}
-
-function tickClient (ip) {
-  clients[ip] -= 1;
-}
-
-function trackNewClient (ip) {
-  clients[ip] = limit;
-}
-function checkOk (ip) {
-  if (clients[ip] < 1) {
-    return false;
-  } else {
-    return true;
+  if (!client) {
+    clients[name] = client = new Client(name, type, config[type].every);
   }
+
+  res.setHeader('X-RateLimit-Limit', config[type].totalRequests);
+  res.setHeader('X-RateLimit-Remaining', config[type].totalRequests - client.ticks);
+
+  if (ok(client)) {
+    client.ticks++;
+    next();
+  } else {
+    res.statusCode = 429;
+    res.end('Rate limit exceded.');
+  }
+}
+
+function Client (name, type, resetIn) {
+  var name   = name;
+
+  this.name  = name;
+  this.type  = type;
+  this.ticks = 1;
+
+  setTimeout(function () {
+    delete clients[name];
+  }, resetIn);
+}
+
+function ok (client) {
+  console.log(client);
+  if (client.type === 'whitelist') {
+    return client.ticks <= config.whitelist.totalRequests;
+  }
+  if (client.type === 'blacklist') {
+    return client.ticks <= config.blacklist.totalRequests;
+  }
+  if (client.type === 'normal') {
+    return client.ticks <= config.normal.totalRequests;
+  }
+}
+
+function getClientType (name) {
+  if (whitelist.indexOf(name) > -1) {
+    return 'whitelist';
+  }
+  if (blacklist.indexOf(name) > -1) {
+    return 'blacklist';
+  }
+  return 'normal';
 }
